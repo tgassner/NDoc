16fe75e273b9aa24f34648e6380bb47fc555fceb
Thomas Gassner <tgassner@users.noreply.github.com>
2008-04-18 10:08:52 +0000

    first VS - Add-In Experiments

---
+++ ndoc/src/NDocVisualStudioAddIn/SolutionPlugin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NDocVisualStudioAddIn
+{
+    public class SolutionPlugin : NDoc.VisualStudio.ISolution
+    {
+        public string Directory
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ICollection<NDoc.VisualStudio.ConfigurationElements> GetConfigurations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<string> GetConfigurationsNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NDoc.VisualStudio.IProject GetProject(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProjectConfigName(string solutionConfig, string projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<NDoc.VisualStudio.IProject> GetProjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public NDoc.VisualStudio.IdeType Ide
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int ProjectCount
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
