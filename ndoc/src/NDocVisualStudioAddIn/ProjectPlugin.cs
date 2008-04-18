c8ac40590e67f2bc7aaee490259eb5ffeaa9fc54
Gassi <Gassi@743bd9bb-27ab-734d-ac7d-360be489b5b5>
2008-04-18 10:08:52 +0000

    first VS - Add-In Experiments

---
+++ ndoc/src/NDocVisualStudioAddIn/ProjectPlugin.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NDocVisualStudioAddIn
+{
+    public class ProjectPlugin : NDoc.VisualStudio.IProject{
+
+        public string AssemblyName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public NDoc.VisualStudio.IProjectConfig GetConfiguration(string configName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRelativeOutputPathForConfiguration(string configName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRelativePathToDocumentationFile(string configName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid ID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string OutputFile
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string OutputType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public NDoc.VisualStudio.ProjektType Type
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Read(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RelativePath
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string RootNamespace
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public NDoc.VisualStudio.ISolution Solution
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
