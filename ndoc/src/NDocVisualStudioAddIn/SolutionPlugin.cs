387a3f87a9e668f9cd78665961dbe1f9c83c1eb0
Thomas Gassner <tgassner@users.noreply.github.com>
2008-04-18 21:05:20 +0000

    Beginning readding current Solution and Project Data winth the Add-In

--- ndoc/src/NDocVisualStudioAddIn/SolutionPlugin.cs
+++ ndoc/src/NDocVisualStudioAddIn/SolutionPlugin.cs
@@ -1,54 +1,101 @@
 ﻿using System;
+using EnvDTE;
+using EnvDTE80;
 using System.Collections.Generic;
 using System.Text;
+using NDoc.VisualStudio;
 
-namespace NDocVisualStudioAddIn
-{
-    public class SolutionPlugin : NDoc.VisualStudio.ISolution
-    {
-        public string Directory
-        {
-            get { throw new NotImplementedException(); }
+namespace NDocVisualStudioAddIn {
+    public class SolutionPlugin : NDoc.VisualStudio.ISolution {
+
+        private DTE2 _applicationObject;
+        protected IDictionary<Guid, IProject> _projects = new Dictionary<Guid, IProject>();
+
+        public SolutionPlugin(DTE2 _applicationObject) {
+            this._applicationObject = _applicationObject;
+
+            ///TODO: Projects Extrahieren und in Dictionary speichern!
         }
 
-        public ICollection<NDoc.VisualStudio.ConfigurationElements> GetConfigurations()
-        {
+
+        public string Directory {
+            get {
+                return System.IO.Path.GetDirectoryName(this._applicationObject.Solution.FullName);
+            }
+        }
+
+        public ICollection<NDoc.VisualStudio.ConfigurationElements> GetConfigurations() {
             throw new NotImplementedException();
         }
 
-        public ICollection<string> GetConfigurationsNames()
-        {
+        public ICollection<string> GetConfigurationsNames() {
+            IList<string> solCfgs = new List<string>();
+            IList<string> solPltfrms = new List<string>();
+            IList<string> solCfgsAndPltfrms = new List<string>();
+
+            foreach (SolutionConfiguration solCfg in _applicationObject.Solution.SolutionBuild.SolutionConfigurations) {
+                if (!solCfgs.Contains(solCfg.Name)) {
+                    solCfgs.Add(solCfg.Name);
+                }
+                foreach (SolutionContext solContext in solCfg.SolutionContexts) {
+                    if (!solPltfrms.Contains(solContext.PlatformName)) {
+                        solPltfrms.Add(solContext.PlatformName);
+                    }
+                }
+            }
+
+
+            foreach (string configs in solCfgs) {
+                foreach (string platforms in solPltfrms) {
+                    solCfgsAndPltfrms.Add(configs + "|" + platforms);
+                }
+            }
+
+            return solCfgsAndPltfrms;
+        }
+
+        public NDoc.VisualStudio.IProject GetProject(string name) {
             throw new NotImplementedException();
+            //this._applicationObject.Solution.Projects.Item(0).
         }
 
-        public NDoc.VisualStudio.IProject GetProject(string name)
-        {
+        public string GetProjectConfigName(string solutionConfig, string projectId) {
             throw new NotImplementedException();
+            //IDictionary<Guid, string> ce = _configurationAndPlatforms[solutionConfig];
+            //if (ce == null) {
+            //    return null;
+            //} else {
+            //    return ce[new Guid(projectId)];
+            //}
+            return null;
         }
 
-        public string GetProjectConfigName(string solutionConfig, string projectId)
-        {
-            throw new NotImplementedException();
+        public ICollection<NDoc.VisualStudio.IProject> GetProjects() {
+            return _projects.Values;
         }
 
-        public ICollection<NDoc.VisualStudio.IProject> GetProjects()
-        {
-            throw new NotImplementedException();
+        public string Name {
+            get {
+                return System.IO.Path.GetFileNameWithoutExtension(this._applicationObject.Solution.FullName);
+            }
         }
 
-        public string Name
-        {
-            get { throw new NotImplementedException(); }
+        public NDoc.VisualStudio.IdeType Ide {
+            get {
+                switch (this._applicationObject.Version) {
+                    case "9.0": return NDoc.VisualStudio.IdeType.Studio2008;
+                        break;
+                    case "8.0": return NDoc.VisualStudio.IdeType.Studio2005;
+                        break;
+                    default: return NDoc.VisualStudio.IdeType.Unknown;
+                }
+            }
         }
 
-        public NDoc.VisualStudio.IdeType Ide
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public int ProjectCount
-        {
-            get { throw new NotImplementedException(); }
+        public int ProjectCount {
+            get {
+                return _projects.Count;
+            }
         }
     }
 }
