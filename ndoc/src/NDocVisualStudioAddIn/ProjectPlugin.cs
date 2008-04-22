92616a08b3bc8e4fa44cebabc67db431586fc0d6
Thomas Gassner <tgassner@users.noreply.github.com>
2008-04-22 13:37:13 +0000

    AddIn becomming more features

--- ndoc/src/NDocVisualStudioAddIn/ProjectPlugin.cs
+++ ndoc/src/NDocVisualStudioAddIn/ProjectPlugin.cs
@@ -1,81 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using EnvDTE80;
 
-namespace NDocVisualStudioAddIn
-{
-    public class ProjectPlugin : NDoc.VisualStudio.IProject{
+namespace NDocVisualStudioAddIn {
+    public class ProjectPlugin : NDoc.VisualStudio.IProject {
 
-        public string AssemblyName
-        {
-            get { throw new NotImplementedException(); }
+        private DTE2 _applicationObject;
+        private string _uniqueName;
+
+        public ProjectPlugin(DTE2 _applicationObject, string uniqueName) {
+            this._applicationObject = _applicationObject;
+            this._uniqueName = uniqueName;
         }
 
-        public NDoc.VisualStudio.IProjectConfig GetConfiguration(string configName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetRelativeOutputPathForConfiguration(string configName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetRelativePathToDocumentationFile(string configName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Guid ID
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string Name
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string OutputFile
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string OutputType
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public NDoc.VisualStudio.ProjektType Type
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public void Read(string path)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string RelativePath
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
+        public string AssemblyName {
+            get {
+                EnvDTE.Project proj = this._applicationObject.Solution.Projects.Item(_uniqueName);
+                return proj.Name;
             }
         }
 
-        public string RootNamespace
-        {
+        public NDoc.VisualStudio.IProjectConfig GetConfiguration(string configName) {
+            throw new NotImplementedException();
+        }
+
+        public string GetRelativeOutputPathForConfiguration(string configName) {
+            //return Path.Combine(
+            //                Path.Combine(FullPath, GetConfiguration(configName).OutputPath),
+            //                OutputFileName);
+            return string.Empty;
+        }
+
+        public string GetRelativePathToDocumentationFile(string configName) {
+            throw new NotImplementedException();
+        }
+
+        public Guid ID {
             get { throw new NotImplementedException(); }
         }
 
-        public NDoc.VisualStudio.ISolution Solution
-        {
+        public string Name {
+            get {
+                EnvDTE.Project proj = this._applicationObject.Solution.Projects.Item(_uniqueName);
+                return proj.Name;
+            }
+        }
+
+        public string OutputFile {
+            get {
+                return this.getProjectProperty(_uniqueName, "OutputFileName");
+            }
+        }
+
+        public string OutputType {
             get { throw new NotImplementedException(); }
+        }
+
+        public NDoc.VisualStudio.ProjektType Type {
+            get {
+                EnvDTE.Project proj = this._applicationObject.Solution.Projects.Item(_uniqueName);
+                return NDoc.VisualStudio.Project.GetProjectType(proj.Kind);
+            }
+        }
+
+        public void Read(string path) {
+            throw new NotImplementedException();
+        }
+
+        public string RelativePath {
+            get {
+                return this.getProjectProperty(_uniqueName, "FullPath");
+            }
+            set {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string RootNamespace {
+            get {
+                return this.getProjectProperty(_uniqueName, "RootNamespace");
+            }
+        }
+
+        public NDoc.VisualStudio.ISolution Solution {
+            get { throw new NotImplementedException(); }
+        }
+
+        private string getProjectProperty(string projectUniqueName, string key) {
+            try {
+                EnvDTE.Project prj = _applicationObject.Solution.Projects.Item(projectUniqueName);
+                return prj.Properties.Item(key).Value.ToString();
+            } catch (Exception) {
+                return string.Empty;
+            }
         }
     }
 }
